ab912e3dc8a85e7527ac76b4d0e42c50582efd63
giovannirp <giovanni_ge@hotmail.com>
2026-09-17 14:34:20 -0300

    feat: adicionando exercicio dobro de numero

---
+++ Operacao-de-entrada-saida/exercicio-dobro-numero.py
@@ -0,0 +1,12 @@
+# Faça um programa que peça um número ao input
+# E mostre o dobro desse número
+
+# Entrada de dados
+numero = int(input("Digite um número: "))
+
+# Processamento computacional
+dobro = numero * 2
+
+# Saída de informações
+print(f"Número digitado: {numero}")
+print(f"Dobro: {dobro}")
