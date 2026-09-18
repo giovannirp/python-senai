676b959b93b5f865ba1e723b6402d17e95973804
giovannirp <giovanni_ge@hotmail.com>
2026-09-18 14:52:54 -0300

    feat: adicionando add exercicios

---
+++ Estrutura-de-decisao/04-exercicio-verificacao-passagem.py
@@ -0,0 +1,8 @@
+# Programa: Verificação de passagem
+
+# Faça um programa em Python que peça a idade de uma pessoa
+# e verifique se ela paga passagem inteira ou meia passagem.
+
+# Idade menor ou igual a 12 → Meia passagem
+# Idade maior que 12 → Passagem inteira
+
